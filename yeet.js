5236a6cbe1c1fe16e2c560b86d1e3cd6dabd8eba
ridderp <47546481+ridderp@users.noreply.github.com>
2019-02-11 20:46:56 -0500

    Add files via upload

---
+++ yeet.js
@@ -0,0 +1,35 @@
+alert ("yeet");
+console.time("Clock");
+console.log("nein");
+console.log(1234647685);
+
+var edge = "chrome";
+console.log(edge);
+console.error("fix it boi");
+console.warn("be careful");
+console.clear();
+console.timeEnd("Clock");
+
+const julio = ["yeeeeee, nooooooooooo, okkkkkkkkkkkk"];
+console.log(julio);
+julio[1] = "haaaaaaaaaaa";
+console.log(julio);
+const ja = [12, 45, 67, 98, 100];
+console.log(ja)
+ja[3] = 99;
+console.log(ja);
+
+var you = 600
+var value = 599.99;
+Math.round(value);
+var me = 
+console.log(me)
+
+var no = Math.max(8, 9);
+console.log(no);
+
+var hall = "ball";
+var jall = "call";
+var sumWords = hall + ", " + jall;
+console.log(sumWords);
+
